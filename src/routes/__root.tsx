2251b8c67a34eb56d791818b18fae3179b07bc08
lijing191 <lijing191@tal.com>
2026-01-12 17:59:51 +0800

    feat

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -16,13 +16,18 @@ export const Route = createRootRoute({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "TanStack Start Starter",
+				title: "Awesome Mac",
 			},
 		],
 		links: [
 			{
 				rel: "stylesheet",
 				href: appCss,
+			},
+			{
+				rel: "icon",
+				href: "/mac.svg",
+				type: "image/svg+xml",
 			},
 		],
 	}),
